0df8a7fd7a43b6a081b58228e3c00153b3ffbe70
josehoon <josehoon@familysearch.org>
2018-02-15 08:59:21 -0700

    add test

---
+++ src/test/java/myTestPackage/HelloTest.java
@@ -0,0 +1,12 @@
+package myTestPackage;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class HelloTest {
+  @Test
+  public void testDoFakeTest() throws Exception {
+
+    assertTrue(true);
+  }
+}
